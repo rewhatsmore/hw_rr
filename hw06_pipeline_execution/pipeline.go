946a33e7b1e0a22c7ac8b660adcda02c1da3b33c
rewhatsmore <rewhatsmore@gmail.com>
2023-02-08 14:28:45 +0300

    HW6 need help

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,20 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	var out Out = in
+	for _, stage := range stages {
+		bi := make(Bi)
+		go func(in In) {
+			defer close(bi)
+			for v := range in {
+				select {
+				case bi <- v:
+				case <-done:
+					return
+				}
+			}
+		}(out)
+		out = stage(bi)
+	}
+	return out
 }
